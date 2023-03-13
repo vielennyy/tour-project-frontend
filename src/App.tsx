f34f78e76486ebd6080672be4037b19927da533b
Illia <ddallmour@gmail.com>
2023-03-13 23:42:06 +0200

    Create login popup

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,23 @@ import { HomePage } from "./pages/HomePage";
 import { ToursPage } from './pages/ToursPage';
 import { UserPage } from "./pages/UserPage";
 import { Footer } from './components/Footer';
-import { JsxElement } from 'typescript';
 
+import 'normalize.css';
+import {Box} from "@mui/material";
 
 function App():JSX.Element {
   return (
-	<>
-	<NavBar/>
-			<Routes>
-			<Route path="/" element={<HomePage />} />
-			<Route path="/user" element={<UserPage />} />
-			<Route path="/tours" element={<ToursPage />} />
-			<Route path="/accommodations" element={<h1>Accommodations</h1>} />
-			<Route path="/foods" element={<h1>Foods</h1>} />
-			</Routes>
-	<Footer/>
-	</>
+	<Box>
+		<NavBar/>
+				<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="/user" element={<UserPage />} />
+				<Route path="/tours" element={<ToursPage />} />
+				<Route path="/accommodations" element={<h1>Accommodations</h1>} />
+				<Route path="/foods" element={<h1>Foods</h1>} />
+				</Routes>
+		<Footer/>
+	</Box>
   );
 }
 
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -4,15 +4,12 @@ import {HomePageContent} from "../../components/HomePageContent";
 
 export const HomePage = () => {
   return(
+
     <Box sx={{
       width: {xs: '100%', md: '1024px'},
       m: '0 auto'
     }}>
-      <Box>
-      </Box>
-      <Box>
-        <HomePageContent/>
-      </Box>
+      <HomePageContent/>
     </Box>
   )
   }
