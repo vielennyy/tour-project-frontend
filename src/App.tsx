96097bfc42e5619a1c39a88a7d20055766ae293a
Illia <kravchenkoillia95@gmail.com>
2023-04-03 23:59:57 +0300

    Homepage authorization

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,27 @@
+import { createContext, useState, useContext, useMemo } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import {Box} from "@mui/material";
 
 import {Layout} from "./pages/Layout";
 import AppRoutes from "./AppRoutes"
 
+import {UserContext} from "./UserContext";
+
 function App():JSX.Element {
+  const [isAuthorize, setIsAuthorize] = useState(false);
 
   return (
 	<Box>
-
-    <Layout>
-      <Routes>
-        {AppRoutes.map((route, index) => {
-          const { element, ...rest } = route;
-          return <Route key={index} {...rest} element={element} />;
-        })}
-      </Routes>
-    </Layout>
+    <UserContext.Provider value={{isAuthorize, setIsAuthorize}}>
+      <Layout>
+        <Routes>
+          {AppRoutes.map((route, index) => {
+            const { element, ...rest } = route;
+            return <Route key={index} {...rest} element={element}/>;
+          })}
+        </Routes>
+      </Layout>
+    </UserContext.Provider>
 	</Box>
   );
 }
